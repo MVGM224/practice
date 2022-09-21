a90c9bfb6f2223bab4587e1aac885869dccb7491
janani <janani.jeyaram@particleblack.com>
2022-09-21 12:18:12 -0700

    landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
