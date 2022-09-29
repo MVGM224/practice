5d5cff3c811b8b11d63890233182578d4040ca1e
muralivijay~ <muralivijay.ravindran@particleblack.com>
2022-09-29 09:56:08 +0530

    landing form - post method - completed

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,48 +1,55 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  selector: "app-landing-page",
+  templateUrl: "./landing-page.component.html",
+  styleUrls: ["./landing-page.component.scss"],
 })
 export class LandingPageComponent implements OnInit {
-  landingform: FormGroup
+  landingform: FormGroup;
   // arrSave: Array<any>=[];
   text:any;
   userList: any;
 
   constructor(private http:HttpClient) { }
 
-
   ngOnInit() {
-    this.landingform=new FormGroup({
-      project:new FormControl(),
-      team:new FormControl(),
-      status:new FormControl(),
-      createdOn:new FormControl(),
-      createdBy:new FormControl()
+    this.landingform = new FormGroup({
+      project: new FormControl(),
+      team: new FormControl(),
+      status: new FormControl(),
+      createdOn: new FormControl(),
+      createdBy: new FormControl(),
     });
     //this.arrSave=[{"project":"Cardinality" , "status":}]
-    this.http.get('https://api-demo-trainee-dev.cardinalityai.xyz/project').subscribe(res=>{
-      this.userList=res;
-    })
+    this.http
+      .get("https://api-demo-trainee-dev.cardinalityai.xyz/project")
+      .subscribe((res) => {
+        this.userList = res;
+      });
   }
 
-  save(){
-    this.http.post('https://api-demo-trainee-dev.cardinalityai.xyz/project/create',{...this.landingform.value}).subscribe(res=>{
-      if(res){
-        console.log("Success")
-      }
-    })
+  save() {
+
+    this.http.post("https://api-demo-trainee-dev.cardinalityai.xyz/project/create",
+     {...this.landingform.value,  })
+      .subscribe((res) => {
+        if (res) {
+          console.log("Success");
+          this.http.get("https://api-demo-trainee-dev.cardinalityai.xyz/project")
+          .subscribe((res) => {
+          this.userList = res;
+      });
+          this.landingform.reset();
+          document.getElementById('cancelbutton').click();
+        }
+      });
     // this.arr_save && this.arr_save.push(this.landingform.value)
-// this.text=this.landingform.value;
-// this.arrSave && this.arrSave.push(this.text);
+    // this.text=this.landingform.value;
+    // this.arrSave && this.arrSave.push(this.text);
 
-//     console.log("Success",this.arrSave);
-
+    //     console.log("Success",this.arrSave);
   }
-
-
 }
